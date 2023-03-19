3a857b80049120d6788a641531c10485e276bc01
HugoHLZhang <hugo.zhang@edu.esiee.Fr>
2023-03-19 22:24:42 +0100

    Add Nerd Thumbnail and Optimisation du code player movement et InworldPlayer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Inworld.Runtime;
+using Inworld.Sample.UI;
+using Inworld.Util;
 public class PlayerMovement : MonoBehaviour
 {
 	public float runSpeed = 10f;
 
 	Vector2 direction;
-	bool chatOpen;
 	Rigidbody2D rb;
     void Start()
     {
 		rb = GetComponent<Rigidbody2D>();
-		chatOpen = false;
     }
-    // Update is called once per frame
-    void Update()
-	{
-		if (Input.GetKeyUp(KeyCode.BackQuote) && !chatOpen)
-		{
-			chatOpen = true;
-		}
-		else if (Input.GetKeyUp(KeyCode.BackQuote) && chatOpen)
-        {
-			chatOpen = false;
-        }
-
-	}
-
-	void FixedUpdate()
+    void FixedUpdate()
 	{
 		// Move our character
-		if (!chatOpen)
+		if (!Inworld.Sample.InworldPlayer.m_chatIsOpened)
         {
 			direction.x = Input.GetAxisRaw("Horizontal") * runSpeed;
 			direction.y = Input.GetAxisRaw("Vertical") * runSpeed;
