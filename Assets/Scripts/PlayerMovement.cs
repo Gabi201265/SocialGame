04adc742b8aa7bec6942b970744e879edde05dfd
gabriel.leroux@edu.esiee.fr <gabriel.leroux@edu.esiee.fr>
2023-03-27 08:59:41 +0200

    Rajout des animations pour le perso

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,9 +11,11 @@ public class PlayerMovement : MonoBehaviour
 	public float runSpeed = 5f;
 	Vector2 direction;
 	Rigidbody2D rb;
+	Animator anim;
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		anim= GetComponent<Animator>();
 	}
 	void FixedUpdate()
 	{
@@ -23,6 +25,33 @@ public class PlayerMovement : MonoBehaviour
 			direction.x = Input.GetAxisRaw("Horizontal") * runSpeed;
 			direction.y = Input.GetAxisRaw("Vertical") * runSpeed;
 			rb.MovePosition(rb.position + direction * Time.fixedDeltaTime);
+
+			SetParam();
+
+		}
+	}
+
+	void SetParam()
+    {
+		if (direction.x == 0 && direction.y == 0)
+		{
+			anim.SetInteger("dir", 0);
+		}
+        else if (direction.x > 0) //droite
+        {
+			anim.SetInteger("dir", 4);
+		}
+		else if (direction.x < 0) //gauche
+		{
+			anim.SetInteger("dir", 2);
+		}
+		else if (direction.y > 0) //haut
+		{
+			anim.SetInteger("dir", 3);
+		}
+		else if (direction.y < 0) //bas
+		{
+			anim.SetInteger("dir", 1);
 		}
 	}
 }
